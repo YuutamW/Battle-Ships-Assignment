36dc9250ca4cd232cbf87d07bc59daa0be6abddb
noameil <noam.eilat@gmail.com>
2026-01-10 00:01:22 +0200

    Human Player done

---
+++ HumanPlayer.cpp
@@ -0,0 +1,74 @@
+#include "HumanPlayer.hpp"
+
+void HumanPlayer::placeAllShips()
+{
+    int row, col;
+    char orientation;
+    bool horizontal;
+    int i = 0;
+    while (i < SHIPSAMOUNT)
+    {
+        std::cout << "Enter tile number for the " << getShip(i)->getName() << ": ex. 1 1" << std::endl;
+        std::cin >> row >> col;
+        if (!VALIDGRIDINP(row, col))
+        {
+            std::cout << "Invalid row/column" << std::endl;
+            continue;
+        }
+        std::cout << "Enter desired orientation: H for horizontal, V for vertical" << std::endl;
+        std::cin >> orientation;
+        switch (orientation)
+        {
+        case 'H':
+            horizontal = true;
+            break;
+
+        case 'h':
+            horizontal = true;
+            break;
+
+        case 'V':
+            horizontal = false;
+            break;
+
+        case 'v':
+            horizontal = false;
+            break;
+
+        default:
+            break;
+        }
+
+        if (!getGrid().inBounds(row, col, getShip(i)->getSize(), horizontal))
+        {
+            std::cout << "Ship placement is out of bounds, try again" << std::endl;
+            continue;
+        }
+        else if (!getGrid().isTileOccupied(row, col))
+        {
+            std::cout << "Tile is already occupied, try again" << std::endl;
+            continue;
+        }
+        else
+        {
+            getGrid().placeShip(row, col, getShip(i)->getSize(), horizontal);
+        }
+        i++;
+    }
+}
+
+void HumanPlayer::makeMove(Player *opponent)
+{
+    int row, col;
+    std::cout << "Enter tile number for your next move: ex. 1 1" << std::endl;
+    std::cin >> row >> col;
+    if (!VALIDGRIDINP(row, col))
+    {
+        std::cout << "Invalid row/column, try again" << std::endl;
+        makeMove(opponent);
+    }
+    if (opponent->getGrid().getCell(row, col) == 'S')
+    {
+        opponent->getGrid().findShipInGrid(row, col).takeHit();
+    }
+}
--- Ship.hpp
+++ Ship.hpp
@@ -10,8 +10,11 @@ private:
   int hitsTaken;
 
 public:
-  Ship(string &Name, int shipSize) : name(Name), size(shipSize) ,hitsTaken(0) {}  
+  Ship(string &Name, int shipSize) : name(Name), size(shipSize), hitsTaken(0) {}
   virtual ~Ship() = 0;
-  virtual void takeHit() {hitsTaken++;};
-  inline bool isSunk() const {return  hitsTaken >= size;}
+  virtual void takeHit() { hitsTaken++; };
+  inline bool isSunk() const { return hitsTaken >= size; }
+
+  inline int getSize() { return size; };
+  inline string getName() { return name; }
 };
